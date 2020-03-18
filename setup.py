c65819100343a4068fa5e63f4462866094b99974
qizhicheng <qizhicheng@douban.com>
2020-03-18 16:55:39 +0800

    support py3.6+ only

--- setup.py
+++ setup.py
@@ -68,10 +68,7 @@ setup(name='sa-tools-core',
       # https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Development Status :: 5 - Production/Stable',
