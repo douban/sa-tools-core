ccb4bbaa27585fd5eee07a8856034186cbdc420e
everpcpc <git@everpcpc.com>
2020-04-12 09:29:37 +0800

    chore: cleanup qcloudsdk

--- sa_tools_core/libs/qcloud/qcloudsdkbm/DescribeDeviceClassRequest.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from ..qcloudsdkcore.request import Request
-
-class DescribeDeviceClassRequest(Request):
-
-    def __init__(self):
-        super(DescribeDeviceClassRequest, self).__init__(
-            'bm', 'qcloudcliV1', 'DescribeDeviceClass', 'bm.api.qcloud.com')
