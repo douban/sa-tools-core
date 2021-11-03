898500c04a4d7392b97836522e729138c145a044
LeoQuote <leo.quote@hotmail.com>
2021-11-03 12:43:22 +0800

    bump version
    add lark multi tenant support

--- sa_tools_core/libs/notify/lark.py
+++ sa_tools_core/libs/notify/lark.py
@@ -8,6 +8,7 @@ import logging
 import pylark
 
 from sa_tools_core.utils import get_config
+from sa_tools_core.consts import DEFAULT_LARK_TENANT
 
 logger = logging.getLogger(__name__)
 
@@ -15,13 +16,47 @@ APP_ID = "aaaaaaaaaaaaaaaaaa"
 APP_SECRET = "bbbbbbbbbbbbbbbbbbbbbb"
 
 
+class LarkApp(object):
+    tenants = {}
+
+    def __init__(self, lark=None, lark_ark=None):
+        self.tenants["douban"] = lark
+        self.tenants["ark"] = lark_ark
+
+    @staticmethod
+    def load_from_configs(configs: dict):
+        app = LarkApp()
+        for name, config in configs.items():
+            lark = pylark.Lark(
+                app_id=config["app_id"],
+                app_secret=config["app_secret"]
+            )
+            app.tenants[name] = lark
+        return app
+
+    def get_lark(self, company="douban"):
+        lark = self.tenants.get(company)
+        if lark:
+            return lark
+        logger.error(f"no tenant named {company}, please check your code and configs")
+
+
 def send_message(addrs, content, **kwargs):
-    secret = get_config("lark").split(":")
-    APP_ID = secret[0]
-    APP_SECRET = secret[1]
-    cli = pylark.Lark(app_id=APP_ID, app_secret=APP_SECRET)
+    secret = get_config("lark-multi-tenant")
+    # config template:
+    # {
+    #     "your_company": {
+    #        "app_id": "1",
+    #        "app_secret": "1",
+    #        "encrypt_key": "1",
+    #        "verification_token": "1"
+    #    }
+    # }
+    lark_bundle = LarkApp.load_from_configs(configs=json.loads(secret))
+    company = kwargs.get("company", DEFAULT_LARK_TENANT)
+    lark = lark_bundle.get_lark(company=company)
     for addr in addrs:
-        res, response = cli.message.send_raw_message(
+        res, response = lark.message.send_raw_message(
             pylark.SendRawMessageReq(
                 receive_id_type="email",
                 receive_id=addr,
